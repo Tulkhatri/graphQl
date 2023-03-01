16046f9ff9a05eedf9eba8d6fea35df2c54b167c
Tulkhatri <tulkhatri01@gmail.com>
2023-03-01 16:52:41 +0545

    connect database with cloud.mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const colors=require('colors')
 require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema')
+const connectDB=require('./config/db')
 const port = process.env.PORT || 5000;
 const app = express();
+connectDB();
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: process.env.NODE_ENV === 'development'
