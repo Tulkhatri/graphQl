2b153f88c7ed2bc0be494c227732bf3e7897e2f9
Tulkhatri <tulkhatri01@gmail.com>
2023-03-01 15:40:13 +0545

    GraphQl project setup with sample data

---
+++ server/src/schema/schema.js
@@ -0,0 +1,69 @@
+const { projects, clients } = require('../sampleData.js')
+const { GraphQLObjectType,
+     GraphQLID, 
+     GraphQLString,
+     GraphQLSchema,
+     GraphQLList,
+    } = require('graphql')
+
+//Project Types
+const ProjectType = new GraphQLObjectType({
+    name: 'Projcet',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+        client:{
+            type:ClientType,
+            resolve(parent,args){
+                return clients.find(client=>client.id===parent.clientId);
+
+            }
+        }
+    })
+});
+//client Types
+const ClientType = new GraphQLObjectType({
+    name: 'Client',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        email: { type: GraphQLString },
+        phone: { type: GraphQLString },
+    })
+});
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        projects:{
+            type: new GraphQLList(ProjectType),
+            resolve(parent,args){
+                return projects;
+            }
+        },
+        project: {
+            type: ProjectType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                return projects.find(project=>project.id===args.id);
+            }
+        },
+        clients:{
+            type: new GraphQLList(ClientType),
+            resolve(parent,args){
+                return clients;
+            }
+        },
+        client: {
+            type: ClientType,
+            args:{id:{type:GraphQLID}},
+            resolve(parent,args){
+                return clients.find(client=>client.id===args.id);
+            }
+        }
+    }
+});
+module.exports=new GraphQLSchema({
+    query:RootQuery
+})
